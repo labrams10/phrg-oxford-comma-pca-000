b67e5ef5c9e8af6140fcf6620d8d3cb423abb295
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-11 18:31:44 -0400

    finished oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" " "and" " ")
+  else array.count >= 3
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    array.join(", ")
+  end
 
 end
